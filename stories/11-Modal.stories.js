638d2b98b1d0cb25adaf84d1f10c98c691f0480f
Brent Eckert <brent.eckert7@gmail.com>
2022-01-24 17:40:17 -0700

    refactor: adding components section to stories

--- stories/11-Modal.stories.js
+++ stories/11-Modal.stories.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { Modal } from '../components';
+import React from "react";
+import { Modal } from "../components";
 
 export default {
-  title: 'Modal',
+  title: "components/Modal",
   component: Modal,
 };
 
 export const ModalNormal = () => {
   return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', width: '100%' }}>
-    <Modal>
-      <h1>This is a normal modal</h1>
-    </Modal>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <Modal>
+        <h1>This is a normal modal</h1>
+      </Modal>
     </div>
   );
 };
